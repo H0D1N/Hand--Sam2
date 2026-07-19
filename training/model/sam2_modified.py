7c5582ea33cab0ab2a5bf648f4d540806ceed343
Xintu Zhao <zhaoxintu0330@163.com>
2026-07-19 20:38:21 +0800

    模型：为左右解码器分别生成高分辨率特征

--- training/model/sam2_modified.py
+++ training/model/sam2_modified.py
@@ -17,3 +17,48 @@ class SAM2Modified(SAM2Train):
         del self.sam_mask_decoder
 
         assert (self.left_mask_decoder is not self.right_mask_decoder)
+
+    def forward_image(self, img_batch):
+
+        backbone_out = self.image_encoder(img_batch)
+
+        if self.use_high_res_features_in_sam:
+            backbone_fpn = backbone_out["backbone_fpn"]
+
+            left_high_res_features = (
+                self._project_high_res_features(
+                    backbone_fpn=backbone_fpn,
+                    mask_decoder=self.left_mask_decoder,
+                )
+            )
+
+            right_high_res_features = (
+                self._project_high_res_features(
+                    backbone_fpn=backbone_fpn,
+                    mask_decoder=self.right_mask_decoder,
+                )
+            )
+        else:
+            left_high_res_features = None
+            right_high_res_features = None
+
+        backbone_out["left_high_res_features"] = (
+            left_high_res_features
+        )
+
+        backbone_out["right_high_res_features"] = (
+            right_high_res_features
+        )
+
+        return backbone_out
+    
+
+    def _project_high_res_features(
+        self,
+        backbone_fpn,
+        mask_decoder,
+    ):
+        feature_s0 = mask_decoder.conv_s0(backbone_fpn[0])
+        feature_s1 = mask_decoder.conv_s1(backbone_fpn[1])
+
+        return [feature_s0, feature_s1]
